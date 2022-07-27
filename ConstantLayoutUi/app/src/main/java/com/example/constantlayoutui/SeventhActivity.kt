a5434ad50944b1a98dc76b85bfe09f82472ca0b4
jeni kachhadiya <kachhadiyajeni0001@gmail.com>
2022-07-28 00:05:35 +0530

    add new project

---
+++ ConstantLayoutUi/app/src/main/java/com/example/constantlayoutui/SeventhActivity.kt
@@ -0,0 +1,11 @@
+package com.example.constantlayoutui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeventhActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seventh)
+    }
+}
